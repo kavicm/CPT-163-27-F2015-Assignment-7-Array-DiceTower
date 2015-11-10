1a104d9b803c557a70ae3746b95efb2e1a890ed2
kavicm <kavicm@my.wccc.edu>
2015-11-10 17:02:06 -0500

    Is this better?
    I tried again less cheating.  Just needed some help with the constructors.

--- Assignment7/src/assignment7/DiceTower.java
+++ Assignment7/src/assignment7/DiceTower.java
@@ -19,42 +19,51 @@ List <Die> dice;
 private int trayValue;
 private int value;
 
+int[] rolls = new int[PANNELS];//create array list
+
 /**
  * Constructor
  */
-public DiceTower(){
-    this.dice = new ArrayList();
-    value = 0;
-    trayValue = 0;
-}
+public DiceTower() {
+        this.dice = new ArrayList(); // 
+        this.trayValue = 0;
+    }
 /**
  * Constructor
  * @param dice 
  */
-public DiceTower (List dice){
-        this.dice = dice;
-        value = 0;
-        trayValue = 0;
+public DiceTower(List dice) {
+        this.dice = dice; // 
+        this.trayValue = 0;
     }
  
 /**
  * dropDice method
  */
-    public void dropDice(){
-        for (int i = 0; i < dice.size(); i++){
-            for(int p =0; p <PANELS; p++){
-            this.dice.get(i).roll();
-        }
-        trayValue += this.dice.get(i).value();
-        }
-    }
+    public void dropDice()
+        {
+       for (int index = 0; index < PANNELS; index ++ )
+       {
+        Die die = new Die(6);
+	die.roll(); 
+        rolls[index] = die.value();
+       }
+       
+       
+   }
     
    /**
      * trayValue method
      * @return sum of the dice rolls
      */
-    public int trayValue(){
-        return trayValue;
+   public int trayValue( )
+   {
+       int sum = 0;
+       for (int i =0; i<rolls.length; i++)
+       {
+           sum += i;
+       }
+       return sum;
     }
     }
 
